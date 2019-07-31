35f643678f429e7d2b6d97b7b1229352a3a6a640
Jordan Ng <ng.jordanm@gmail.com>
2019-07-31 00:55:07 -0400

    add allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vdv+i3mn8o_7^vs-$p6_r=(-nje+8ex7alaa6##6oq1kzi8ut_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '.kayleighv.com']
 
 
 # Application definition
